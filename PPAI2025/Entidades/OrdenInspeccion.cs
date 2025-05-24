c559b1f8c6c0cefd775d27d7b749dec1f4a614cd
Pablo Caranta <Pablo Caranta@PABLO-PC>
2025-05-24 19:44:44 -0300

    Agregar archivos de proyecto.

---
+++ PPAI2025/Entidades/OrdenInspeccion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI2025.Entidades
+{
+    public class OrdenInspeccion
+    {
+        // Atributos clase
+        private int nroOrden;
+        private string observacionCierre;
+        private DateTime fechaHoraInicio;
+        private DateTime fechaHoraCierre;
+        private DateTime fechaHoraFinalizacion;
+
+        // Atributos referencia
+        private Estado Estado;
+        private Empleado Empleado;
+        private EstacionSismologica EstacionSismologica;
+
+
+        // Constructor
+        public OrdenInspeccion(int nroOrd, string obsCierre, DateTime fechaHIni, DateTime fechaHCierre, DateTime fechaHFinaliz)
+        {
+            nroOrden = nroOrd;
+            observacionCierre = obsCierre;
+            fechaHoraCierre = fechaHCierre;
+            fechaHoraInicio = fechaHIni;
+            fechaHoraFinalizacion = fechaHFinaliz;
+        }
+
+        public int NroOrden { get { return nroOrden;} set { nroOrden = value} }
+        public string ObservacionCierre { get { return observacionCierre; } set { observacionCierre = value; } }
+        public DateTime FechaHoraInicio { get { return fechaHoraInicio; } set { fechaHoraInicio = value; } } 
+        public DateTime FechaHoraCierre { get { return fechaHoraCierre; } set { fechaHoraInicio = value; } }
+        public DateTime FechaHoraFinalizacion { get { return fechaHoraFinalizacion; } set {fechaHoraFinalizacion = value; } }
+        public Estado Estado { get { return Estado; } set { Estado = value; } }
+        public Empleado Empleado { get { return Empleado; } set { Empleado = value; } }
+        public EstacionSismologica EstacionSismologica { get { return   EstacionSismologica; } set { EstacionSismologica = value; } }
+
+
+    }
+
+}
---
+++ PPAI2025/Gestor e interfaces/GestorCerrarOrdenInspeccion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI2025.Gestor_e_interfaces
+{
+    internal class GestorCerrarOrdenInspeccion
+    {
+    }
+}
